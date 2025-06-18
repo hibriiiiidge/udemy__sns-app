f699b04d881e66739fa6d7826d9fcc60f69adbbd
Yasunori Takahashi <hibriiiiidge@gmail.com>
2025-06-19 05:55:55 +0900

    Set routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:udemy__sns_app/screens/home_screen.dart';
+import 'package:udemy__sns_app/screens/signin_screen.dart';
+import 'package:udemy__sns_app/screens/signup_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const SnsApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class SnsApp extends StatefulWidget {
+  const SnsApp({super.key});
 
   @override
+  SnsAppState createState() => SnsAppState();
+}
+
+class SnsAppState extends State<SnsApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: const Color(0xFF34D399),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF34D399),
+          elevation: 0,
         ),
       ),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(
+              builder: (context) => const HomeScreen(),
+            );
+          case '/signin':
+            return MaterialPageRoute(
+              builder: (context) => const SigninScreen(),
+            );
+          case '/signup':
+            return MaterialPageRoute(
+              builder: (context) => const SignupScreen(),
+            );
+          default:
+            return MaterialPageRoute(
+              builder: (context) => const HomeScreen(),
+            );
+        }
+      },
+      home: const SigninScreen(),
     );
   }
 }
