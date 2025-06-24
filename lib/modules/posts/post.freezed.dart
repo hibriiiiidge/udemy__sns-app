d64a1df0702382a29c9657f33aebd4b7e7580d36
Yasunori Takahashi <hibriiiiidge@gmail.com>
2025-06-25 05:53:25 +0900

    Add post class

---
+++ lib/modules/posts/post.freezed.dart
@@ -0,0 +1,157 @@
+// dart format width=80
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'post.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+// dart format off
+T _$identity<T>(T value) => value;
+
+/// @nodoc
+mixin _$Post {
+
+ String get id; String get content; String get userId; String get userName;
+/// Create a copy of Post
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$PostCopyWith<Post> get copyWith => _$PostCopyWithImpl<Post>(this as Post, _$identity);
+
+  /// Serializes this Post to a JSON map.
+  Map<String, dynamic> toJson();
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is Post&&(identical(other.id, id) || other.id == id)&&(identical(other.content, content) || other.content == content)&&(identical(other.userId, userId) || other.userId == userId)&&(identical(other.userName, userName) || other.userName == userName));
+}
+
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,id,content,userId,userName);
+
+@override
+String toString() {
+  return 'Post(id: $id, content: $content, userId: $userId, userName: $userName)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $PostCopyWith<$Res>  {
+  factory $PostCopyWith(Post value, $Res Function(Post) _then) = _$PostCopyWithImpl;
+@useResult
+$Res call({
+ String id, String content, String userId, String userName
+});
+
+
+
+
+}
+/// @nodoc
+class _$PostCopyWithImpl<$Res>
+    implements $PostCopyWith<$Res> {
+  _$PostCopyWithImpl(this._self, this._then);
+
+  final Post _self;
+  final $Res Function(Post) _then;
+
+/// Create a copy of Post
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? id = null,Object? content = null,Object? userId = null,Object? userName = null,}) {
+  return _then(_self.copyWith(
+id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as String,content: null == content ? _self.content : content // ignore: cast_nullable_to_non_nullable
+as String,userId: null == userId ? _self.userId : userId // ignore: cast_nullable_to_non_nullable
+as String,userName: null == userName ? _self.userName : userName // ignore: cast_nullable_to_non_nullable
+as String,
+  ));
+}
+
+}
+
+
+/// @nodoc
+@JsonSerializable()
+
+class _Post implements Post {
+  const _Post({required this.id, required this.content, required this.userId, required this.userName});
+  factory _Post.fromJson(Map<String, dynamic> json) => _$PostFromJson(json);
+
+@override final  String id;
+@override final  String content;
+@override final  String userId;
+@override final  String userName;
+
+/// Create a copy of Post
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$PostCopyWith<_Post> get copyWith => __$PostCopyWithImpl<_Post>(this, _$identity);
+
+@override
+Map<String, dynamic> toJson() {
+  return _$PostToJson(this, );
+}
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _Post&&(identical(other.id, id) || other.id == id)&&(identical(other.content, content) || other.content == content)&&(identical(other.userId, userId) || other.userId == userId)&&(identical(other.userName, userName) || other.userName == userName));
+}
+
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,id,content,userId,userName);
+
+@override
+String toString() {
+  return 'Post(id: $id, content: $content, userId: $userId, userName: $userName)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class _$PostCopyWith<$Res> implements $PostCopyWith<$Res> {
+  factory _$PostCopyWith(_Post value, $Res Function(_Post) _then) = __$PostCopyWithImpl;
+@override @useResult
+$Res call({
+ String id, String content, String userId, String userName
+});
+
+
+
+
+}
+/// @nodoc
+class __$PostCopyWithImpl<$Res>
+    implements _$PostCopyWith<$Res> {
+  __$PostCopyWithImpl(this._self, this._then);
+
+  final _Post _self;
+  final $Res Function(_Post) _then;
+
+/// Create a copy of Post
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? id = null,Object? content = null,Object? userId = null,Object? userName = null,}) {
+  return _then(_Post(
+id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
+as String,content: null == content ? _self.content : content // ignore: cast_nullable_to_non_nullable
+as String,userId: null == userId ? _self.userId : userId // ignore: cast_nullable_to_non_nullable
+as String,userName: null == userName ? _self.userName : userName // ignore: cast_nullable_to_non_nullable
+as String,
+  ));
+}
+
+
+}
+
+// dart format on
